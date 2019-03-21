cae0c3a30071fb9a1220fa08a78a5c018ffaee90
Len Damico <len@arcweb.co>
2019-03-20 20:52:21 -0400

    draw 5 cards

--- 02/demo.js
+++ 02/demo.js
@@ -1,14 +1,11 @@
 'use strict';
+const _ = require('underscore')
+
 const sentences = [
   { subject: 'JavaScript', verb: 'is', object: 'great'},
   { subject: 'Elephants', verb: 'are', object: 'large'},
 ];
-// es 6 feature: object destructuring
-function say ({subject, verb, object}) {
-  // es6 feature: template strings
-  console.log(`${subject} ${verb} ${object}`);
+const verbs = _.pluck(sentences, 'verb')
+for(let verb of verbs) {
+  console.log(`found verb: ${verb}`)
 }
-  //es6 feature: for..of
-  for(let s of sentences) {
-    say(s);
-  }
