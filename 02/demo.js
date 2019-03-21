7c810b3541ed03bd567f8afa8b661ce1b759eb37
Len Damico <len@arcweb.co>
2019-03-20 20:41:18 -0400

    beginning of Node app

---
+++ 02/demo.js
@@ -0,0 +1,14 @@
+'use strict';
+const sentences = [
+  { subject: 'JavaScript', verb: 'is', object: 'great'},
+  { subject: 'Elephants', verb: 'are', object: 'large'},
+];
+// es 6 feature: object destructuring
+function say ({subject, verb, object}) {
+  // es6 feature: template strings
+  console.log(`${subject} ${verb} ${object}`);
+}
+  //es6 feature: for..of
+  for(let s of sentences) {
+    say(s);
+  }
